ea259da7685f08a7ad88954ef56ecd9cd3ec14e8
Gean Alexandre da Silva <gean.silva@programmers.com.br>
2018-02-28 09:58:09 -0300

    added route

--- WatchTyping.Infra/WatchTypingExtension.cs
+++ WatchTyping.Infra/WatchTypingExtension.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using WatchTyping.Infra.CrossCutting;
+using WatchTyping.Infra.Services;
 
 namespace WatchTyping.Infra
 {
     public static class WatchTypingExtension
     {
-        public static void UseWatchTyping(this IServiceCollection serviceCollection)
+        public static void UseWatchTyping(this IServiceCollection services)
         {
-            DependecyInject.Resolve(serviceCollection);
+            DependecyInject.Resolve(services);
+        }
+
+        public static void ConfigureWatchTypingRoute(this IApplicationBuilder app, string route)
+        {
+            app.UseSignalR(routes => routes.MapHub<SignalRHubService>(route));
         }
     }
 }
